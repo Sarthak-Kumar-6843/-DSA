58e12dda3185ade74a9f5d6ec1cb9c89fcb2468b
Sarthak-kumar-6843 <sarthakkumar.k6843@gmail.com>
2025-07-29 23:49:45 +0530

    Update

--- OrderAgnosticBS.java
+++ OrderAgnosticBS.java
@@ -7,32 +7,31 @@ public class OrderAgnosticBS {
     static int orderAgnosticBS(int[] arr , int target){
         int start = 0;
         int end = arr.length - 1;
-
-        //find whether the array is ascending or descending
+        //find whether array is ascending or descending
         boolean isAsc = arr[start] < arr[end];
 
+        while(start <= end){
+            //find middle element
+            int mid = start + (end - start)/2;
 
-        while (start <= end){
-            //find mid
-            int mid = start + (end - start) / 2;
             if(arr[mid] == target){
                 return mid;
             }
-            if(isAsc){
+
+            if (isAsc){
                 if(target < arr[mid]){
                     end = mid - 1;
-                } else {
+                }else {
                     start = mid + 1;
                 }
             }else {
                 if(target > arr[mid]){
                     end = mid - 1;
-                } else {
+                }else {
                     start = mid + 1;
                 }
             }
-
         }
-        return  -1;
+        return -1;
     }
 }
