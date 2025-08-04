19eb47569930da3b55fa49bdd0e5ec39327e7530
Sarthak-kumar-6843 <sarthakkumar.k6843@gmail.com>
2025-08-05 01:29:50 +0530

    Update

--- RotationCount.java
+++ RotationCount.java
@@ -1,6 +1,6 @@
 public class RotationCount {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
+        int[] arr = {3,4,5,6,7,0,1,2};
         System.out.println(countRotations(arr));
     }
 
